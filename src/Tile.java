3a3e66ac03126a2fb01e70e21b51352562b1dd6d
DanOujesky <oujesky@spsejecna.cz>
2024-03-27 21:20:07 +0100

    changed methods to static methods

--- src/Tile.java
+++ src/Tile.java
@@ -3,14 +3,14 @@ import java.awt.*;
 
 public class Tile {
 
-    final ImageIcon GRASS = new ImageIcon("pictures/GRASS.png");
-    final ImageIcon DIRT = new ImageIcon("pictures/DIRT.png");
+    final static ImageIcon GRASS = new ImageIcon("pictures/GRASS.png");
+    final static ImageIcon DIRT = new ImageIcon("pictures/DIRT.png");
 
 
-    public Image getGRASS() {
+    public static Image getGRASS() {
         return GRASS.getImage();
     }
-    public Image getDIRT() {
+    public static Image getDIRT() {
         return DIRT.getImage();
     }
 }
