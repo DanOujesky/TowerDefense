0fafeaa2fdd517af34635f8709ae5658d235a1bb
DanOujesky <oujesky@spsejecna.cz>
2024-03-27 21:16:47 +0100

    added method to return position of the first grass

--- src/Background.java
+++ src/Background.java
@@ -3,11 +3,12 @@ import java.awt.image.BufferedImage;
 
 public class Background {
     private static final int TILESIZE = 60;
+    private static final int columnLenght = TILESIZE / 4;
     Tile tile = new Tile();
     public void draw(Graphics2D graphics2D) {
 
-        for (int x = 0; x < TILESIZE/4; x++) {
-            for (int y = 0; y < TILESIZE/4; y++) {
+        for (int x = 0; x < columnLenght; x++) {
+            for (int y = 0; y < columnLenght; y++) {
                 switch (backgroundEditor[x][y]) {
                     case 1:
                         graphics2D.drawImage(tile.getGRASS(), TILESIZE*y, TILESIZE*x, null);
@@ -37,6 +38,15 @@ public class Background {
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
     };
+    public int positionOfFirstTile(){
+        int tilePosition;
+        for (tilePosition = 0; tilePosition < columnLenght; tilePosition++) {
+            if (backgroundEditor[0][tilePosition] == 2) {
+                break;
+            }
+        }
+        return tilePosition;
+    }
 
 
 
