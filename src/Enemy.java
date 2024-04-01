8537723667b681eaf13a6b73153b6841841dd39e
DanOujesky <oujesky@spsejecna.cz>
2024-04-01 10:52:34 +0200

    added method to rotate Enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,38 +1,106 @@
-import javax.swing.*;
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Enemy {
-    private int x,y;
+    private double x,y;
+    private boolean death = false;
+    final static double movementSpeed = 5;
     private int direction;
-    final static ImageIcon ENEMY_1 = new ImageIcon("pictures/Enemies/Enemy_1.png");
+    final static BufferedImage ENEMY_1;
+
+    static {
+        try {
+            ENEMY_1 = ImageIO.read(new File("pictures/Enemies/Enemy_1.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public Enemy(){
-        x = 0;
+        x = -Background.TILESIZE;
         y = Background.positionOfFirstTile();
         direction = 1;
+        spawn();
+    }
+    public void spawn(){
+        while (x == 0) {
+            update();
+        }
     }
     public void draw(Graphics2D graphics2D) {
-        graphics2D.drawImage(ENEMY_1.getImage(), x,y, null);
+        switch (direction) {
+            case 1:
+                graphics2D.drawImage(rotateImage(ENEMY_1, 0), (int) x, (int) y, null);
+                break;
+            case 2:
+                graphics2D.drawImage(rotateImage(ENEMY_1, 270), (int) x, (int) y, null);
+                break;
+            case 3:
+                graphics2D.drawImage(rotateImage(ENEMY_1, 180),(int) x, (int) y, null);
+                break;
+            case 4:
+                graphics2D.drawImage(rotateImage(ENEMY_1, 90), (int) x, (int) y, null);
+                break;
+        }
     }
 
     public void update() {
         switch (direction) {
             case 1:
-                x += 1;
+                x += movementSpeed;
                 break;
             case 2:
-                y -= 1;
+                y -= movementSpeed;
                 break;
             case 3:
-                x -= 1;
+                x -= movementSpeed;
                 break;
             case 4:
-                y += 1;
+                y += movementSpeed;
                 break;
         }
-        if (!Background.isNextDirt(x, y, direction) && x % 60 == 0 && y % 60 == 0) {
-            chooseDirection();
+        if (x < GamePanel.WIDTH - Background.TILESIZE * 1.5) {
+            if (!Background.isNextDirt(x, y, direction) && x % Background.TILESIZE == 0 && y % Background.TILESIZE == 0) {
+                chooseDirection();
+            }
+        } else if (x > (GamePanel.WIDTH - ENEMY_1.getWidth() + Background.TILESIZE)) {
+            death = true;
         }
     }
+    private static BufferedImage rotateImage(BufferedImage buffImage, double angle) {
+        double radian = Math.toRadians(angle);
+        double sin = Math.abs(Math.sin(radian));
+        double cos = Math.abs(Math.cos(radian));
+
+        int width = buffImage.getWidth();
+        int height = buffImage.getHeight();
+
+        int nWidth = (int) Math.floor((double) width * cos + (double) height * sin);
+        int nHeight = (int) Math.floor((double) height * cos + (double) width * sin);
+
+        BufferedImage rotatedImage = new BufferedImage(
+                nWidth, nHeight, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D graphics = rotatedImage.createGraphics();
+
+        graphics.setRenderingHint(
+                RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+
+        graphics.translate((nWidth - width) / 2, (nHeight - height) / 2);
+        graphics.rotate(radian, (double) (width / 2), (double) (height / 2));
+        graphics.drawImage(buffImage, 0, 0, null);
+        graphics.dispose();
+
+        return rotatedImage;
+    }
+
+    public boolean isDeath() {
+        return death;
+    }
+
     public void chooseDirection(){
         int pastDirection = direction;
         switch (direction) {
@@ -73,7 +141,6 @@ public class Enemy {
                 }
                 break;
         }
-
 
     }
 
