cecf5672711ad8360c8b863d4fb19c1ffa55632f
DanOujesky <oujesky@spsejecna.cz>
2024-04-06 22:49:13 +0200

    added new class MyMouseListener

---
+++ src/MyMouseListener.java
@@ -0,0 +1,45 @@
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class MyMouseListener implements MouseListener, MouseMotionListener {
+    public static double positionX;
+    public static double positionY;
+    public static boolean mousePressed;
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        mousePressed = true;
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        mousePressed = false;
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        positionX = e.getX();
+        positionY = e.getY();
+    }
+}
