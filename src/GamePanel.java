a98f1e68e645d8eddac36a77ba5fa05d291d7420
DanOujesky <oujesky@spsejecna.cz>
2024-04-06 22:47:55 +0200

    added new Arraylist towers and update and draw all the elements

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.TimerTask;
 
@@ -13,16 +17,20 @@ public class GamePanel extends JPanel implements Runnable{
     public static int COINS = 0;
     private boolean gameOver = false;
     Thread gameThread;
-    PlayManager playmanager;
     Background background;
     private static ArrayList<Enemy> enemies = new ArrayList<>();
+    private static ArrayList<Tower> towers = new ArrayList<>();
+    JButton myButton = new JButton("Button");
     public GamePanel(){
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setLayout(null);
+        this.addMouseListener(new MyMouseListener());
+        this.addMouseMotionListener(new MyMouseListener());
+        this.add(myButton);
 
-        playmanager = new PlayManager();
         background = new Background();
-
+        myButton.setBounds(60, 750, 100, 100);
+        myButton.addActionListener(new MyButtonListener(0,0,5,5,10, new File("pictures/Towers/Tower_1.png")));
     }
     public void addEnemy(String name){
         switch (name) {
@@ -36,6 +44,9 @@ public class GamePanel extends JPanel implements Runnable{
                 enemies.add(new Enemy(new File("pictures/Enemies/Enemy_3.png"), 5, 10,1, 8));
                 break;
         }
+    }
+    public static void addTower(Tower tower){
+        towers.add(tower);
     }
     public void launchGame(){
         gameThread = new Thread(this);
@@ -81,22 +92,36 @@ public class GamePanel extends JPanel implements Runnable{
 
     }
     public void update(){
-        playmanager.update();
         for (int i =0; i < enemies.size(); i++) {
             if (enemies.get(i).isDeath() == true) {
                 enemies.remove(enemies.get(i));
             }
             enemies.get(i).update();
         }
+        for (int i =0; i < towers.size(); i++) {
+            towers.get(i).update();
+        }
+
     }
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
 
         Graphics2D graphics2D = (Graphics2D) graphics;
-        playmanager.draw(graphics2D);
         background.draw(graphics2D);
+        try {
+            graphics2D.drawImage(ImageIO.read(new File("pictures/Health_Bar/Health_Bar_" + HEALTH + ".png")), -40,-80, 240, 240, null);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         for (int i =0; i < enemies.size(); i++) {
             enemies.get(i).draw(graphics2D);
         }
+        for (int i =0; i < towers.size(); i++) {
+            towers.get(i).update();
+        }
+        for (int i =0; i < towers.size(); i++) {
+            towers.get(i).draw(graphics2D);
+        }
+
     }
 }
