66e5670e8dafdc4db08761cf0008835a43e6028a
DanOujesky <oujesky@spsejecna.cz>
2024-04-07 21:08:53 +0200

    added new class Waves

---
+++ src/Waves.java
@@ -0,0 +1,60 @@
+import org.ietf.jgss.GSSManager;
+
+import java.util.ArrayList;
+
+public class Waves {
+    static int timer;
+    private final static int SECOND = 60;
+    static int enemiesCount;
+    static boolean wave = false;
+    static int waveCount = 0;
+    static ArrayList<String> enemiesTypes = new ArrayList<>();
+    public static void startWave(){
+        timer = 0;
+        enemiesCount = 0;
+        wave = true;
+        waveCount++;
+        enemiesTypes.clear();
+    }
+    public static void update(){
+        timer++;
+        switch (waveCount){
+            case 1:
+                wave(15);
+                wave1();
+        }
+
+    }
+    public static void wave(int fullEnemies){
+        if (enemiesCount >= fullEnemies) {
+            wave = false;
+            GamePanel.myWaveButton.setVisible(true);
+        }
+    }
+    public static int countEnemyTypes(String enemyName){
+        int count = 0;
+        for (int i =0; i < enemiesTypes.size(); i++) {
+            if (enemiesTypes.get(i).equals(enemyName)) count++;
+        }
+        return count;
+    }
+    public static void spawnEnemy(double spawnRate, String enemyName, int maxEnemyOFType){
+        if (spawnRatePerSecond(spawnRate) && countEnemyTypes(enemyName) < maxEnemyOFType){
+            GamePanel.addEnemy(enemyName);
+            enemiesTypes.add(enemyName);
+            enemiesCount++;
+        }
+    }
+    public static boolean spawnRatePerSecond(double seconds) {
+        if (timer % (SECOND * seconds) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void wave1(){
+        spawnEnemy(1,"enemy1",5);
+        spawnEnemy(0.2,"enemy2",10);
+    }
+
+}
