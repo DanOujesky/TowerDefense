1a97c824e37c35e86a52b78c203b643fc9379eda
DanOujesky <oujesky@spsejecna.cz>
2024-04-25 16:33:42 +0200

    added if to check if we have money on tower

--- src/MyTowerButtonListener.java
+++ src/MyTowerButtonListener.java
@@ -6,18 +6,25 @@ import java.io.File;
 
 public class MyTowerButtonListener implements ActionListener {
     private double x,y, attackSpeed, damage, prize;
+    String name;
+    int range;
     File towerFile;
-    public MyTowerButtonListener(double x, double y, double attackSpeed, double damage, double prize, File towerFile){
+    public MyTowerButtonListener(double x, double y, double attackSpeed, double damage, double prize, File towerFile, String name, int range){
         this.x = x;
         this.y = y;
         this.attackSpeed = attackSpeed;
         this.damage = damage;
         this.prize = prize;
         this.towerFile = towerFile;
+        this.name = name;
+        this.range = range;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        Tower tower = new Tower(x,y,attackSpeed,damage,prize,towerFile);
-        tower.setPlaceTower(true);
+        if (CoinBar.COINS >= prize) {
+            Tower tower = new Tower(x,y,attackSpeed, damage,prize,towerFile, name, range);
+            tower.setPlaceTower(true);
+        }
+
     }
 }
