23389a228c8dbff6de3e7817a207326dabf57847
DanOujesky <oujesky@spsejecna.cz>
2024-04-25 16:27:03 +0200

    added new class CoinBar

---
+++ src/CoinBar.java
@@ -0,0 +1,16 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class CoinBar {
+    public static int COINS;
+    public CoinBar(int COINS) {
+        this.COINS = COINS;
+    }
+
+    public void draw(Graphics2D graphics2D) {
+        String coins = "COINS: " + COINS;
+        graphics2D.drawString(coins, 800, 40);
+    }
+}
