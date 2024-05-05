1e41403f229c766b4be8ef00154b29b42cdfac4b
DanOujesky <oujesky@spsejecna.cz>
2024-05-05 18:43:55 +0200

    added method to change icon

--- src/MyUpgradeButton.java
+++ src/MyUpgradeButton.java
@@ -1,5 +1,7 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -17,25 +19,38 @@ public class MyUpgradeButton extends JButton {
         setIcon(upgradeButtonImage);
         this.setBorder(BorderFactory.createEmptyBorder());
         this.setContentAreaFilled(false);
-        this.setBounds((int) tower.getX()-30, (int) tower.getY()-30-(tower.getRange()/2), upgradeButtonImage.getIconWidth(),upgradeButtonImage.getIconHeight());
+        this.setBounds((int) tower.getX()-15, (int) tower.getY()-(tower.getRange()/2)-15, upgradeButtonImage.getIconWidth()/2,upgradeButtonImage.getIconHeight()/2);
         this.addActionListener(new MyUpgradeButtonListener(this));
     }
     public void changeIcon(){
-        switch (clickCount) {
-            case 0:
-                setIcon(upgradeButtonImage);
-                break;
-            case 1:
-                setIcon(upgradeButtonImageClicked);
-                break;
-            case 2:
-                clickCount = 0;
-                setIcon(upgradeButtonImage);
-                tower.upgrade();
-                break;
+        if (tower.level < tower.maxLevel) {
+            switch (clickCount) {
+                case 0:
+                    setIcon(upgradeButtonImage);
+                    break;
+                case 1:
+                    setIcon(upgradeButtonImageClicked);
+                    break;
+                case 2:
+                    if (CoinBar.COINS > tower.getUpgradeValues()[2]) {
+                        CoinBar.COINS -= tower.getUpgradeValues()[2];
+                        setIcon(upgradeButtonImage);
+                        tower.upgrade();
+                    } else {
 
+                    }
+                    clickCount = 0;
+
+                    break;
+
+            }
         }
+
     }
+    public boolean collisionWithMouse(){
+        return this.getBounds().contains(MyMouseListener.positionX, MyMouseListener.positionY);
+    }
+
 
     public int getClickCount() {
         return clickCount;
@@ -43,5 +58,27 @@ public class MyUpgradeButton extends JButton {
 
     public void setClickCount(int clickCount) {
         this.clickCount = clickCount;
+    }
+
+    public void draw(Graphics2D graphics2D) {
+        graphics2D.setColor(Color.black);
+        graphics2D.fillRect(320, 10, 170,100);
+        graphics2D.fillRect(530, 10, 170,100);
+        graphics2D.setFont(new Font("Arial",1,15));
+        graphics2D.setColor(Color.white);
+        drawStringWithEnters(graphics2D,"Damage: " + (int)tower.getDamage() + "\n" + "Attack Speed: " + 60/(int)tower.getAttackSpeed() + "shots/s" + "\n" + "Prize: " + (int)tower.getPrize() + "coins" + "\n" + "Range: " + tower.getRange() , 330, 20);
+        if (tower.level < tower.maxLevel) {
+            graphics2D.setColor(Color.GREEN);
+            drawStringWithEnters(graphics2D,"Damage: " + tower.getUpgradeValues()[0] + "\n" + "Attack Speed: " + 60/tower.getUpgradeValues()[1] + "shots/s" + "\n" + "Prize: " + tower.getUpgradeValues()[2] + "coins" + "\n" + "Range: " + tower.getUpgradeValues()[3] , 540, 20);
+        } else {
+            graphics2D.setFont(new Font("Arial",1,30));
+            drawStringWithEnters(graphics2D, "MAX" + "\n"+ "LEVEL",570,20);
+        }
+
+
+    }
+    private void drawStringWithEnters(Graphics2D graphics2D, String text, int x, int y) {
+        for (String line : text.split("\n"))
+            graphics2D.drawString(line, x, y += graphics2D.getFontMetrics().getHeight());
     }
 }
