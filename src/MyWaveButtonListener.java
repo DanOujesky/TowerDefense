13fa41e945481d8b465d0b3e473f6b272a82e4b7
DanOujesky <oujesky@spsejecna.cz>
2024-05-08 20:21:21 +0200

    added constructor

--- src/MyWaveButtonListener.java
+++ src/MyWaveButtonListener.java
@@ -3,9 +3,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MyWaveButtonListener implements ActionListener {
+
+    Waves waves;
+    MyWaveButton myWaveButton;
+    public MyWaveButtonListener(Waves waves, MyWaveButton myWaveButton){
+        this.waves = waves;
+        this.myWaveButton = myWaveButton;
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
-        GamePanel.myWaveButton.setVisible(false);
-        Waves.startWave();
+        if (myWaveButton.visible) {
+            myWaveButton.setVisible(false);
+            waves.startWave(myWaveButton);
+        }
+
     }
 }
