8ff6025284e25aac14bc8340de90c736195ac568
DanOujeský <ouejsky@spsejecna.cz>
2024-05-23 10:56:15 +0200

    added java docs

--- src/Game/BackgroundButton.java
+++ src/Game/BackgroundButton.java
@@ -3,8 +3,17 @@ package Game;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * this class represents Background button
+ */
+
 public class BackgroundButton extends JButton {
 
+    /**
+     * assign variables
+     * @param gamePanel
+     * @param imageIcon
+     */
     public BackgroundButton(GamePanel gamePanel, String imageIcon){
         this.setText(imageIcon);
         if (imageIcon.equals("MAPA 1")) {
@@ -13,6 +22,7 @@ public class BackgroundButton extends JButton {
             this.setBounds(500,375,300,150);
         }
         this.addActionListener(new BackgroundButtonListener(gamePanel, imageIcon));
+        MyWindow.addButton(this);
     }
 
 }
--- src/Game/BackgroundButtonListener.java
+++ src/Game/BackgroundButtonListener.java
@@ -3,15 +3,30 @@ package Game;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * this class is listener for the BackgroundButton
+ */
+
 public class BackgroundButtonListener implements ActionListener {
 
     GamePanel gamePanel;
     String imageIcon;
 
+    /**
+     * assign variables
+     * @param gamePanel
+     * @param imageIcon
+     */
+
     public BackgroundButtonListener(GamePanel gamePanel, String imageIcon) {
         this.gamePanel= gamePanel;
         this.imageIcon = imageIcon;
     }
+
+    /**
+     * after click on the button it will launch a game
+     * @param e the event to be processed
+     */
 
     @Override
     public void actionPerformed(ActionEvent e) {
